f561dafe4e3c0e7a1aebf1c2cab9a04b22e6b698
Ramses Almeida <rsaccoll@gmail.com>
2020-08-31 15:52:26 -0300

    more way to testing stuff

--- src/clojure_playground/exemplo6.clj
+++ src/clojure_playground/exemplo6.clj
@@ -66,3 +66,39 @@
 
 (println (total-do-pedido pedido))
 
+
+(def pedido {:mochila  {:quantidade 2, :preco 80}
+             :camiseta {:quantidade 3, :preco 40}
+             :chaveiro {:quantidade 1}})
+
+(defn gratuito?
+  [[_ item]]
+  (<= (get item :preco 0) 0))
+
+(println "filtrando gratuitos")
+(println (filter gratuito? pedido))
+
+(defn gratuito?
+  [item]
+  (<= (get item :preco 0) 0))
+
+(println "filtrando gratuitos 1")
+(println (filter (fn [[chave item]] (gratuito? item)) pedido))
+(println (filter #(gratuito? (second %)) pedido))
+
+
+(defn pago?
+  [item]
+  (not (gratuito? item)))
+
+(println (pago? {:preco 50}))
+(println (pago? {:preco 0}))
+
+(println ((comp not gratuito?) {:preco 50}))
+(println ((comp not gratuito?) {:preco 0}))
+
+(def pago? (comp not gratuito?))
+(println (pago? {:preco 50}))
+(println (pago? {:preco 0}))
+
+
